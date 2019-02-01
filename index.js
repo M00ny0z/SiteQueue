48fba56aade3481dbcc1fd4332d870a7c859159b
Manny Munoz <1mannymunoz@gmail.com>
2019-01-31 21:57:39 -0800

    Added re-worked look

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 (function() {
   "use strict";
   const USER_ID = "em66@uw.edu";
+  const TUTOR_ID = "TAem66@uw.edu";
   window.addEventListener("load", main);
 
   function main() {
     let addTo = document.getElementById("add-to");
     let removeFrom = document.getElementById("remove-from");
+    let removeAs = document.getElementById("remove-as");
     addTo.addEventListener("click", addToQueue);
     removeFrom.addEventListener("click", removeFromQueue);
+    removeAs.addEventListener("click", removeAsQueue);
   }
 
   function addToQueue() {
@@ -15,6 +18,13 @@
   }
 
   function removeFromQueue() {
+    let status = document.getElementById("status");
     console.log("removed");
+
+  }
+
+  function removeAsQueue() {
+    console.log("REMOVED BY " + TUTOR_ID);
+    removeFromQueue();
   }
 })();
